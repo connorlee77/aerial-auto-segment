9abe9af0654c70413b5df4f647935ca9f14be1c0
connorlee77 <connorlee77@gmail.com>
2023-10-24 00:59:56 -0700

    c++ code

---
+++ lulc_refinement/dgm/build/CMakeFiles/densecrf_inference.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/densecrf_inference"
+  "../bin/densecrf_inference.pdb"
+  "CMakeFiles/densecrf_inference.dir/densecrf_inference.cc.o"
+  "CMakeFiles/densecrf_inference.dir/densecrf_inference.cc.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/densecrf_inference.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lulc_refinement/dgm/build/CMakeFiles/densecrf_inference.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for densecrf_inference.
+# This may be replaced when dependencies are built.
